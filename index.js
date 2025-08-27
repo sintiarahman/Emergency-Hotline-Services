4dce1a68cd1621cb8643fced119e3b3f8ed453c8
sintiarahman <sintiarahman19@gmail.com>
2025-08-28 00:58:18 +0600

    call-button function added

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ heartCount.textContent=heartCounts;
 
 )
 }
+
+//call-button ...........
+
+let coins=100
+document.getElementById("coin-count").innerText=coins;
+let buttons =document.querySelectorAll(".call-btn")
+
+buttons.forEach(button=>{
+    button.addEventListener("click",function(){
+let card= button.closest(".card")
+let name =card.querySelectorAll("h3")[0].innerText
+let num = card.querySelectorAll("h3")[1].innerText
+if (coins<20){
+    alert("coin-finised!")
+    return;
+}
+coins -=20
+document.getElementById("coin-count").innerText=coins
+alert ("calling" + name + "("+num+")")
+document.getElementById("history-list").innerHTML+=name +"-"+num+"<br>"
+
+    })
+});
